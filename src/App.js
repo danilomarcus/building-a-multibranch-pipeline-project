e2629900cd28b5597c6f332555a96277c55ff9e5
Danilo Marcus <danpayne16@msn.com>
2023-04-18 21:31:32 -0300

    ao vivo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload again. Alterei!
+          Edit <code>src/App.js</code> and save to reload again. Ao vivo!
         </p>
         <a
           className="App-link"
